a53fdfd748a4936f1ccafb00c66a25acf0e3cef9
Zak Hayek <zakhayek1@gmail.com>
2021-01-14 16:55:25 -0800

    Completed seed.js. Database will now seed with 100 random products.

---
+++ database/Product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  brand: String,
+  price: Number,
+  colors: [String],
+  description: String,
+  img_url: String,
+  category: String,
+  rating: Number,
+  reviews: Number,
+  features: [String],
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
             presets: [
               ['@babel/preset-env'],
               ['@babel/preset-react'],
+              ['airbnb'],
             ],
           },
         },
