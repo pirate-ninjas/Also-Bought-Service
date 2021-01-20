8782a769cdb783930b7ff03e433c02668a2ac46e
Zak Hayek <zakhayek1@gmail.com>
2021-01-19 17:03:54 -0800

    Product cards are now laid out horizontally with information. Rating stars now adequately fill depending on the rating.

--- client/components/ItemList.jsx
+++ client/components/ItemList.jsx
@@ -7,6 +7,10 @@ class ItemList extends React.Component {
   }
 
   render() {
+    function getStars(rating) {
+      const percent = Math.floor((rating / 5) * 100);
+      return percent;
+    }
     let { items } = this.props;
     items = items.map((item, idx) => (
       <div className="productcard" key={idx}>
@@ -14,10 +18,19 @@ class ItemList extends React.Component {
         <div className="brandname">{item.brand}</div>
         <div className="productname">{item.name}</div>
         <div className="rating">
-          <span className="stars">
-            ★★★★★
-            {/* {item.rating} */}
+          {/* <span className="starsFilled">
+            {getStars(item.rating).filled}
           </span>
+          <span className="starsBlank">
+            {getStars(item.rating).blank}
+          </span> */}
+          <span className="stars" 
+            style={{
+              background: `linear-gradient(90deg, gold, gold ${getStars(item.rating)}%, transparent, transparent ${getStars(item.rating)}%)`,
+              WebkitTextStroke: '1px goldenrod',
+              WebkitBackgroundClip: 'text',
+              WebkitTextFillColor: 'transparent',
+            }}>★★★★★</span>
           <span className="reviewers">
             (
             {item.reviews}
@@ -33,7 +46,10 @@ class ItemList extends React.Component {
       </div>
     ));
     return (
-      <div className="alsoBought">{items}</div>
+      <div id="alsoliked">
+        <span id="peopleliked">People also liked</span>
+        <div className="alsoLiked">{items}</div>
+      </div>
     );
   }
 }
