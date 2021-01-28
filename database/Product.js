82e741f6aae896db422cf951702936f1c8471251
Zak Hayek <zakhayek1@gmail.com>
2021-01-28 11:54:28 -0800

    Added dockerfile, run.sh, and changed database connection from localhost to database to fit with docker-compose.yml.

--- database/Product.js
+++ database/Product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/alsobought', {
+mongoose.connect('mongodb://database/alsobought', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
