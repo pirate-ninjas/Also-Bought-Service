fe996bd6f284d2e57c8069852031c951ce48a9eb
Zak Hayek <zakhayek1@gmail.com>
2021-01-29 19:31:51 -0800

    Switched to React.lazy for index.jsx.

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import './components/style.css';
+
+const App = lazy(() => import('./components/App'));
 
 ReactDOM.render(<App />, document.getElementById('app2'));
